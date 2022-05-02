a0fe33ca9c6d4b1961172b94a3e31afbbfd97181
WithVikasVerma <vikasverma9278@gmail.com>
2022-05-02 22:17:41 +0530

    Updates

---
+++ precache-manifest.d62301caa742d2d42b5e08f512896440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2f46c89d6966fd658e9b2ad1b06326",
+    "url": "/IDe/index.html"
+  },
+  {
+    "revision": "813e7e6baf9a7bbe56a7",
+    "url": "/IDe/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "e2b53eacdb9805725076",
+    "url": "/IDe/static/css/main.5b6c6991.chunk.css"
+  },
+  {
+    "revision": "813e7e6baf9a7bbe56a7",
+    "url": "/IDe/static/js/2.81968058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IDe/static/js/2.81968058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b53eacdb9805725076",
+    "url": "/IDe/static/js/main.c4001698.chunk.js"
+  },
+  {
+    "revision": "2cc4e72e3397e7e9bd2d",
+    "url": "/IDe/static/js/runtime-main.e57f8db9.js"
+  }
+]);
